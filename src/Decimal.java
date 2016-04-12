a05acd0330765a7f1f1a549ec2599e83694b05c8
Ilia Vovk <vovkilia@gmail.com>
2016-04-12 21:14:38 +0300

    final project v0.03

--- src/Decimal.java
+++ src/Decimal.java
@@ -2,18 +2,28 @@ import java.util.*;
 
 public class Decimal {
 
-    // Перевод любой системы с 2 до 16 в десятичную
-    public int ToDecimal(String number, int system) {
+    // Нужно (было бы неплохо) сделать:
+    //      1. Ограничить диапазон ответа если введ'нное число слишком большое (выводит неправильно значение)
+    //      2. Ограничить ввод числами от 0 до 9 и цифрами A B C D E F
+    //      3. Добавить минус перед значением если введённое число отрицательное
+    //      4.** Добавить вычисление с плавающей точкой
 
-        Map<Character, Integer> map = new HashMap<>();
+    private Map<Character, Integer> map;
+
+    public Decimal() {
+        this.map = new HashMap<>();
         int k = 0;
-        for (char j = '0';j <= '9' ;j++){
-            map.put(j,k++);
+        for (char j = '0'; j <= '9'; j++) {
+            map.put(j, k++);
         }
         for (char i = 'A'; i < 'G'; i++) {
             map.put(i, k++);
         }
-        System.out.println(map);
+    }
+
+    // Перевод любой системы с 2 до 16 в десятичную
+    public int ToDecimal(String number, int system) {
+
         // Разбиваем строку на символы и создаём массив Integer
         List<Integer> arrayInt = new ArrayList<>();
         for (int i = 0; i < number.length(); i++) {
@@ -25,40 +35,31 @@ public class Decimal {
             }
         }
 
-            // Переводим систему в десятичную
-            Collections.reverse(arrayInt);
-            int sum = 0;
-            for (int i = 0; i < arrayInt.size(); i++) {
-                sum += arrayInt.get(i) * (int) Math.pow(system, i);
-            }
-            return sum;
+        // Переводим систему в десятичную
+        Collections.reverse(arrayInt);
+        int sum = 0;
+        for (int i = 0; i < arrayInt.size(); i++) {
+            sum += arrayInt.get(i) * (int) Math.pow(system, i);
         }
+        return sum;
+    }
 
     // Перевод из десятичной в любую выбранную систему с 2 до 16
     public String DecimalTo(int number, int system) {
+
         List<String> array = new ArrayList<>();
         StringBuilder stringBuilder = new StringBuilder();
 
         while (number != 0) {
             int remainder = number % system;
-            if (remainder < 10) {
-                String toString = "" + remainder;
-                array.add(toString);
-            } else if (remainder == 10) {
-                array.add("A");
-            } else if (remainder == 11) {
-                array.add("B");
-            } else if (remainder == 12) {
-                array.add("C");
-            } else if (remainder == 13) {
-                array.add("D");
-            } else if (remainder == 14) {
-                array.add("E");
-            } else if (remainder == 15) {
-                array.add("F");
+            for (Map.Entry<Character, Integer> c : map.entrySet()) {
+                if (remainder == c.getValue()) {
+                    array.add(String.valueOf(c.getKey()));
+                }
             }
             number /= system;
         }
+
         Collections.reverse(array);
         array.forEach(stringBuilder::append);
         return stringBuilder.toString();
