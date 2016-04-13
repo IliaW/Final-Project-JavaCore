d2e9ea7969f1282fd2d7da995368848bda86b029
Ilia Vovk <vovkilia@gmail.com>
2016-04-13 10:18:38 +0300

    v0.04

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Decimal decimal = new Decimal();
+        HideImplementetion hideImplementation = new HideImplementetion();
 
-        // ВСЕГДА вначале заданное число переводим в десятичною систему (ToDecimal)
-        // После - в любую другую до 16 (DecimalTo)
-
-        // Переведём число "ABC" 16-ричной системы в десятичную
-        //"ABC" - число в выбраной системе.    16 - выбранная система
-        System.out.println(decimal.ToDecimal("ABC", 16)); // Получаем ЧИСЛО INT в десятичной системе
-
-        //Переведём число с 10-тичной системы в 11-тиричную.
-        //95842 - число в десятичной системе.     11 - система, в которую переводим
-        System.out.println(decimal.DecimalTo(95842, 11)); // Получаем СТРОКУ STRING в выбранной (11) системе
-
-        //Пример перевода 16-ричной системы в двоичную
-        System.out.println(decimal.DecimalTo(decimal.ToDecimal("FBD", 16), 2));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        String inputNumber = scanner.nextLine();
+        System.out.println("В какой системе это число");
+        int system = scanner.nextInt();
+        String result = hideImplementation.ToBinary(inputNumber,system);
+        System.out.println(result);
+        System.out.println("Переводим назад из двоичной");
+        System.out.println(hideImplementation.BinaryTo(result,system));
     }
 }
